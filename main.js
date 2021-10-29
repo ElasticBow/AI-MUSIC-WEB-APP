49fe8b71ff3d8818db4ffb2c4f302730a3420b7c
ElasticBow <79611589+ElasticBow@users.noreply.github.com>
2021-10-29 16:46:49 -0700

    Add files via upload

--- main.js
+++ main.js
@@ -1,18 +1,18 @@
-song = "";
+song1 = "";
+song2 = "";
 
 leftWristX = 0;
 leftWristY = 0;
 rightWristX = 0;
 rightWristY = 0;
 
-scoreRightWrist = 0;
 scoreLeftWrist = 0;
 
 function preload()
 {
-    song = loadSound("music.mp3");
+    song1 = loadSound("music.mp3");
+    song2 = loadSound("music2.mp3");
 }
-
 
 function setup() {
     canvas = createCanvas(600, 500);
@@ -32,57 +32,23 @@ function modelLoaded() {
 function draw() {
     image(video, 0, 0, 600, 500);
 
+    song1 = "music.mp3";
+
     fill("#FF0000");
     stroke("FF0000");
-
-    if(scoreRightWrist > 0.2)
-    {
-        circle(rightWristX,rightWristY,20);
-
-        if(rightWristY >0 && rightWristY <= 100)
-        {
-            document.getElementById("speed").innerHTML = "speed = 0.5x";
-            song.rate(0.5);
-        }
-        else if(rightWristY >100 && rightWristY <=200)
-        {
-            document.getElementById("speed").innerHTML = "speed = 1x";
-            song.rate(1);
-        }
-        else if(rightWristY >200 && rightWristY <=300)
-        {
-            document.getElementById("speed").innerHTML = "speed = 1.5x";
-            song.rate(1.5);
-        }
-        else if(rightWristY >300 && rightWristY <=400)
-        {
-            document.getElementById("speed").innerHTML = "speed = 2x";
-            song.rate(2);
-        }
-        else if(rightWristY >400)
-        {
-            document.getElementById("speed").innerHTML = "speed = 2.5x";
-            song.rate(2.5);
-        }
-    }
 
     if(scoreLeftWrist > 0.2)
     {
         circle(leftWristX,leftWristY,20);
-        InNumberleftWristY = Number(leftWristY);
-        remove_decimals = floor(InNumberleftWristY);
-        volume = remove_decimals/500;
-        document.getElementById("volume").innerHTML = "Volume = " + volume;
-        song.setVolume(volume);
+        song2.stop();
+    }
+    if(scoreLeftWrist = false)
+    {
+        song1.play()
+        document.getElementById(song_name).innerHTML = "Playiung song1"
     }
 }
 
-function play()
-{
-    song.play();
-    song.setVolume(1);
-    song.rate(1);
-}
 
 function gotPoses(results)
 {
